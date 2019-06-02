2171558b5fa78b3ca342986ca26381e0b74b4a65
1192439407 <1192439407@qq.com>
2019-06-02 13:36:14 +0800

    [feature][location] add readme

---
+++ js/pages/scan/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+export default class Scan extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <View>
+                <Text>扫描二维码</Text>
+            </View>
+        )
+    }
+} 
